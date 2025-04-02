3f4b0f3f15504497745044ff37f0d20b8d2fcff1
suvani-ctrl <sssubbbi1234@gmail.com>
2025-04-02 14:01:36 +0545

    Create CalculatorApp.jsx
    Main for Calculator app .jsx

---
+++ CalculatorApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calculator from './Calculator/Calculator'
+
+const App = () => {
+  return (
+    <div>
+
+<Calculator />
+    </div>
+  )
+}
+
+export default App
